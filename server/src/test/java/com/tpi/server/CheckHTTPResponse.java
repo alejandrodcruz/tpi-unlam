a74fe3b26aaa5454989d091307c65e2b8659fe3b
Nicolas Larsen <nicolas.larsen96@gmail.com>
2024-08-23 13:35:16 -0300

    Initial java project

---
+++ server/src/test/java/com/tpi/server/CheckHTTPResponse.java
@@ -0,0 +1,23 @@
+package com.tpi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldPassIfInitialEndPointIsAlive() {
+        ResponseEntity<String> response =
+                restTemplate.getForEntity("http://localhost:" + port + "/HelloTpiFinal", String.class);
+    }
+}
