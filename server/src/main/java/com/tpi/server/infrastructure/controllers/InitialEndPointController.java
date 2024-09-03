ef65d8bdce21fd5c24137dab6d179791ce8f876b
Nicolas Larsen <nicolas.larsen96@gmail.com>
2024-09-03 15:19:07 -0300

    Backend: initial conection to DB and initias auths controllers

--- server/src/main/java/com/tpi/server/infrastructure/controllers/InitialEndPointController.java
+++ server/src/main/java/com/tpi/server/infrastructure/controllers/InitialEndPointController.java
@@ -1,15 +1,12 @@
 package com.tpi.server.infrastructure.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class InitialEndPointController {
-
-    @RequestMapping("/HelloTpiFinal")
-    public String initialEndPoint() {
-        return "Hello Tpi Final";
-    }
 
     @RequestMapping("/")
     public String tesOKEndPoint() {
