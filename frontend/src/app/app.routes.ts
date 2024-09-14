744c42e7843215f5a14e24742cc25c321ccb0488
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-09-14 01:39:27 -0300

    Arquitectura limpia en Angular 17

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/auth/login/login.component';
+import { RegisterComponent } from './routes/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: 'home', component:HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {path: '',redirectTo:'/home', pathMatch:'full'}
+];
