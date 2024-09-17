cf2993c62853cb2acf2755bec0ebcf88dfef5eae
lucas2206reta <lureta@alumno.unlam.edu.ar>
2024-09-16 22:29:55 -0300

    Dashboard V1
    Version 1.0

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/auth/login/login.component';
 import { RegisterComponent } from './routes/auth/register/register.component';
 import { DashboardComponent } from './routes/dashboard/dashboard/dashboard.component';
+import { SidebarComponent } from './routes/sidebar/sidebar.component';
 
 export const routes: Routes = [
 
   {path: 'dashboard', component:DashboardComponent},
   {path: 'home', component:HomeComponent},
+  {path: 'sidebar', component:SidebarComponent},
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
   {path: '',redirectTo:'/home', pathMatch:'full'}
