5408270e2218f9bfa26886d73a645cf8a5f7fcd4
ViktorAlbertoGuillin <viktorguillin@yahoo.com>
2024-09-19 09:23:32 -0300

    se agrego el dashboard-historico con el componente y modulo

---
+++ frontend/src/app/routes/dashboard-historico/dashboard-historico/dashboard-historico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-historico',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-historico.component.html',
+  styleUrl: './dashboard-historico.component.css'
+})
+export class DashboardHistoricoComponent {
+
+}
