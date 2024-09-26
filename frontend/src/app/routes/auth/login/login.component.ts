b90578b0ab1a1bf46603167f1f28306597e94123
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-09-26 18:12:15 -0300

    Login con validaciones

--- frontend/src/app/routes/auth/login/login.component.ts
+++ frontend/src/app/routes/auth/login/login.component.ts
@@ -1,27 +1,46 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, FormsModule, CommonModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email: string= '';
-password: string= '';
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
 
-constructor(private authService: AuthService, private router: Router){
-}
-onSubmit() {
-  this.authService.login(this.email, this.password).subscribe({
-   next: ()=> this.router.navigate(['/dashboard']),
-   error: (err) => console.error('Login failed', err)
-  })
-}
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Por favor completa todos los campos correctamente.';
+      return;
+    }
+
+    this.authService.login(
+      this.loginForm.get('email')?.value,
+      this.loginForm.get('password')?.value
+    ).subscribe({
+      next: () => {
+        this.successMessage = 'Inicio de sesión exitoso';
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        console.error('Login failed', err);
+        this.errorMessage = 'Error al iniciar sesión. Por favor, verifica tus credenciales.';
+      }
+    });
+  }
 }
