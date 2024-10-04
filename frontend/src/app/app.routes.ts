db18485989df4d02e002f62bd9be3eb6a78cadcb
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-04 17:49:39 -0300

    fix direccionamiento, merge con main

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/auth/login/login.component';
 import { RegisterComponent } from './routes/auth/register/register.component';
 import { DashboardComponent } from './routes/dashboard/dashboard/dashboard.component';
-import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { ReportesComponent } from './routes/reportes/reportes.component';
 import { ReportesHistoricosComponent } from './routes/reportes/reportes-historicos/reportes-historicos.component';
 import { ConfiguracionComponent } from './routes/configuracion/configuracion.component';
@@ -13,14 +12,14 @@ import { DashboardHistoricoComponent } from './routes/dashboard-historico/dashbo
 
 
 export const routes: Routes = [
-
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'landing', component:  LandingPageComponent},
-  /*{ path: '', redirectTo: '/landing', pathMatch: 'full' },*/
+  { path: 'landing', component: LandingPageComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+
   {
-    path: '',
-    component: HomeComponent,  // HomeComponent actúa como layout principal
+    path: 'home',
+    component: HomeComponent,  // HomeComponent actúa como el layout con el sidebar
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
