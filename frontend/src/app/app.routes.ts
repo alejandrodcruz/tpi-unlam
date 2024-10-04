4bc44408d8677431773f85591388849b82c63fb6
lucas2206reta <lureta@alumno.unlam.edu.ar>
2024-10-04 16:32:52 -0300

    ok
    ok

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,9 +8,8 @@ import { ReportesComponent } from './routes/reportes/reportes.component';
 import { ReportesHistoricosComponent } from './routes/reportes/reportes-historicos/reportes-historicos.component';
 import { ConfiguracionComponent } from './routes/configuracion/configuracion.component';
 import { MisDispositivosComponent} from './routes/mis-dispositivos/mis-dispositivos.component';
-import {
-  DashboardHistoricoComponent
-} from "./routes/dashboard-historico/dashboard-historico/dashboard-historico.component";
+import { DashboardHistoricoComponent } from "./routes/dashboard-historico/dashboard-historico/dashboard-historico.component";
+//import "./routes/dashboard-historico/dashboard-historico/dashboard-historico.component";
 import {HuellaCarbonoComponent} from "./routes/huella-carbono/huella-carbono.component";
 import { LandingPageComponent } from './routes/landing-page/landing-page.component';
 
