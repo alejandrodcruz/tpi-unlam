cd61667f406238bef5bbeb33685bcc3541fa3e82
ViktorAlbertoGuillin <viktorguillin@yahoo.com>
2024-10-05 19:38:38 -0300

    se agrego el servicio para enviar al backend las opciones activa de la pagina de alertas

--- frontend/src/app/routes/alerts/alerts.component.ts
+++ frontend/src/app/routes/alerts/alerts.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CardInfoComponent } from "../../core/card/card-info.component";
+import { FormsModule } from "@angular/forms";
+import { AlertsService } from "../../shared/services/alerts.service";
 
 @Component({
   selector: 'app-alerts',
   standalone: true,
-  imports: [],
+  imports: [
+    CardInfoComponent,FormsModule,
+  ],
   templateUrl: './alerts.component.html',
   styleUrl: './alerts.component.css'
 })
 export class AlertsComponent {
+    alertSettings= {
+    excesoConsumo: 300,
+    excesoConsumoActivo: false,
+    sobretensionActivo: false,
+    bajaTensionActivo: false,
+    perdidaEnergiaActivo: false,
+    picoCorrienteActivo: false,
+    temperaturaAltaActivo: false,
+    humedadAltaActivo: false,
+    dispositivoPerdidoActivo: false
+  };
 
+  constructor(private alertService: AlertsService) {}
+
+  onAlertChange() {
+
+    this.alertService.updateAlertSettings(this.alertSettings).subscribe(response => {
+      console.log('Configuración de alertas actualizada:', response);
+    });
+  }
 }
---
+++ frontend/src/app/shared/services/alerts.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertsService {
+
+  private apiUrl = 'http://localhost:8080/alerts';
+
+  constructor(private http: HttpClient) {}
+
+  updateAlertSettings(alertSettings: any): Observable<any> {
+    console.log( 'paso por el servicio ', alertSettings );
+    return this.http.post(`${this.apiUrl}/update-alerts`, alertSettings);
+  }
+}
