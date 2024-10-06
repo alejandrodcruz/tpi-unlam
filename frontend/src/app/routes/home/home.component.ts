e20b1b07e66a7839d6348b8d0bb6282dcf0c8503
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-06 01:18:27 -0300

    casi responsive(borra erro de detectar tamaño en windows, poner el boton el sidebar y no en toolbar o ocultar por completo el boton de hamburquesa cuando es responsive)

--- frontend/src/app/routes/home/home.component.ts
+++ frontend/src/app/routes/home/home.component.ts
@@ -1,27 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../core/sidebar/sidebar.component';
 import { ToolbarComponent } from '../../core/toolbar/toolbar.component';
 import { CommonModule } from '@angular/common';
 
-
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,
-    SidebarComponent,
-    ToolbarComponent,
-    CommonModule
-  ],
+  imports: [RouterModule, SidebarComponent, ToolbarComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isSidebarOpen = true;
+  screenWidth: number = window.innerWidth;
 
-  // Método para alternar el estado del sidebar
+  ngOnInit() {
+    this.onResize();  // Aseguramos que el estado inicial dependa del tamaño de la pantalla
+  }
+
+  // Método para alternar el estado del sidebar manualmente
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  // Detectar cuando cambia el tamaño de la pantalla
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (typeof window !== 'undefined') {
+      this.screenWidth = window.innerWidth;  // Actualiza el ancho de pantalla si estamos en el navegador
+    }
+
+    if (this.screenWidth < 1024) {
+      this.isSidebarOpen = false;
+    } else {
+      this.isSidebarOpen = true;
+    }
   }
 }
