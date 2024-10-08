3caa202f1ed39133b7a7151d3822baa82c6cb08a
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-08 16:29:02 -0300

    introJS incoporacion

--- frontend/src/app/routes/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/routes/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import {SidebarComponent} from "../../../core/sidebar/sidebar.component";
-import {ReportesHistoricosComponent} from "../../reportes/reportes-historicos/reportes-historicos.component";
-import {CardInfoComponent} from "../../../core/card/card-info.component";
-import {CommonModule, NgClass} from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from "../../../core/sidebar/sidebar.component";
+import { ReportesHistoricosComponent } from "../../reportes/reportes-historicos/reportes-historicos.component";
+import { CardInfoComponent } from "../../../core/card/card-info.component";
+import { CommonModule, NgClass } from "@angular/common";
+import introJs from 'intro.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,14 +16,17 @@ import {CommonModule, NgClass} from "@angular/common";
     CommonModule
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'] // Asegúrate de que sea 'styleUrls' (plural) en lugar de 'styleUrl'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit { // Implementa OnInit
   isExpanded1: boolean = false;
   isExpanded2: boolean = false;
   isExpanded3: boolean = false;
   isExpanded4: boolean = false;
 
+  constructor() { }
+
+  // Método para manejar la expansión de tarjetas
   toggleExpand(cardNumber: number) {
     switch (cardNumber) {
       case 1:
@@ -38,5 +42,39 @@ export class DashboardComponent {
         this.isExpanded4 = !this.isExpanded4;
         break;
     }
+  }
+
+
+  ngOnInit() {
+    this.startTour();
+  }
+
+
+  startTour() {
+    const intro = introJs();
+
+    intro.setOptions({
+      steps: [
+        {
+          intro: "Bienvenido al tutorial de la aplicación!"
+        },
+        {
+          element: '#step1',
+          intro: "Este es el primer paso."
+        },
+        {
+          element: '#step2',
+          intro: "Este es el segundo paso."
+        },
+        {
+          element: '#step3',
+          intro: "Este es el tercer paso."
+        }
+      ],
+      showProgress: true,
+      exitOnOverlayClick: false
+    });
+
+    intro.start();
   }
 }
