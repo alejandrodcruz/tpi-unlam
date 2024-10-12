40e7b2566d49fd2bd9d3ed398edc9604a3931dde
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-11 23:09:31 -0300

    loading componente

--- frontend/src/app/core/device-popup/device-popup.component.ts
+++ frontend/src/app/core/device-popup/device-popup.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-device-popup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+            LoadingComponent
+  ],
   templateUrl: './device-popup.component.html',
   styleUrl: './device-popup.component.css'
 })
@@ -13,6 +16,7 @@ export class DevicePopupComponent {
 @Output() closePopup = new EventEmitter<void>();
 
 step:number = 1;
+isLoading: boolean = false;
 
 close() {
   this.closePopup.emit();
@@ -31,6 +35,12 @@ previousStep() {
 }
 
 submitCode() {
+  this.isLoading = true;
+  setTimeout(() => {
+    this.isLoading = false;
+    this.step = 5;
+  }, 4000);
+
   console.log("Código ingresado");
   this.nextStep();
 }
