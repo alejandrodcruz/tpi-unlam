3140434334ddb49ab0f159eda943b7c700770fbd
lucas2206reta <lureta@alumno.unlam.edu.ar>
2024-10-16 18:32:48 -0300

    cambio en horario
    - Cada 30 seg revision

--- frontend/src/app/shared/services/currenttime.service.ts
+++ frontend/src/app/shared/services/currenttime.service.ts
@@ -8,7 +8,7 @@ export class CurrenttimeService {
   constructor() { }
 
   getHoraActual(): Observable<string> {
-    return interval(1000).pipe(
+    return interval(30000).pipe( // 60000 ms = 1 minuto
       map(() => {
         const now = new Date();
         return this.formatearHora(now);
@@ -16,12 +16,11 @@ export class CurrenttimeService {
     );
   }
 
-  // Método para formatear la hora en formato HH:mm:ss
+
   private formatearHora(date: Date): string {
     const horas = this.agregarCero(date.getHours());
     const minutos = this.agregarCero(date.getMinutes());
-    const segundos = this.agregarCero(date.getSeconds());
-    return `${horas}:${minutos}:${segundos}`;
+    return `${horas}:${minutos}`;
   }
 
   // Agregar un cero adelante si el valor es menor a 10
