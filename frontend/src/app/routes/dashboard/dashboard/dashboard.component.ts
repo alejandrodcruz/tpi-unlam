eea3c2a1c9d6fd162d05108c2193a35e630cc3f7
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-20 23:40:45 -0300

    fix de popup de emparejar el device a un user

--- frontend/src/app/routes/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/routes/dashboard/dashboard/dashboard.component.ts
@@ -50,11 +50,6 @@ export class DashboardComponent implements OnInit {
 
     this.getMeasurements();
 
-    const hasDevice = localStorage.getItem('hasDevice');
-    if (hasDevice === 'true') {
-      this.startTour();
-    }
-
     this.userService.selectedDevice$.subscribe(device => {
       this.selectedDevice = device;
       if (this.selectedDevice) {
@@ -77,52 +72,6 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  startTour() {
-    const hasSeenTour = localStorage.getItem('hasSeenTour');
-
-    if (!hasSeenTour) {
-      const intro = introJs();
-      intro.setOptions({
-        nextLabel: 'Siguiente',
-        prevLabel: 'Atrás',
-        doneLabel: 'Finalizar',
-        exitOnEsc: true,
-        exitOnOverlayClick: false
-      });
-
-      intro.start();
-
-      intro.setOptions({
-        steps: [
-          {intro: "👋 ¡Bienvenido al Dashboard de Lytics! Aquí puedes ver toda la información de consumo energético de tu hogar."
-          },
-          {element: '#step1', intro: "🕒 Reloj – Indica el horario actual en Buenos Aires."},
-          {element: '#step2', intro: "💧 Humedad – Muestra el nivel actual de humedad relativa en el ambiente."},
-          {element: '#step3', intro: "🌡️ Temperatura – Indica la temperatura ambiente en grados Celsius."},
-          {element: '#step4', intro: "💵 Consumo Real – Visualiza el consumo eléctrico actual expresado en kilovatios-hora."},
-          {element: '#step5', intro: "PANEL – Información en tiempo real proporcionada por tu dispositivo."},
-          {element: '#step6', intro: "⚡ Voltaje – Visualiza la tensión eléctrica que llega a tus dispositivos."},
-          {element: '#step7', intro: "🔌 Amperaje – Refleja la cantidad de corriente eléctrica fluyendo en el sistema.\n"},
-          {element: '#step8', intro: "💡 Watts – Indica el consumo instantáneo de energía, similar al combustible utilizado en tiempo real."},
-          {element: '#step9', intro: "⚙️ Kilovatios-hora (kWh) – Mide el consumo acumulado de electricidad durante un período específico."},
-          {element: '#step5', intro: "Ahora podés visualizar detalladamente tus consumos de energía en tiempo real."}
-
-        ],
-        showProgress: true,
-        exitOnOverlayClick: false
-      });
-
-      intro.start();
-
-      intro.oncomplete(() => {
-        localStorage.setItem('hasSeenTour', 'true');
-      });
-
-      intro.onexit(() => {
-        localStorage.setItem('hasSeenTour', 'true');
-      });
-    }
-  }
   getMeasurements() {
     const userId = this.authService.getUserId();
     const fields = ['voltage', 'current', 'power', 'energy']; //campos espesificos
