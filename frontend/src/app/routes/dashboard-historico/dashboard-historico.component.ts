ea0caa8fd8857b10578f0fe370f374f67e1d0e80
Victor Guillin <viktorguillin@yahoo.com>
2024-10-22 15:55:58 -0300

    Agregue las urls para los graficos

--- frontend/src/app/routes/dashboard-historico/dashboard-historico.component.ts
+++ frontend/src/app/routes/dashboard-historico/dashboard-historico.component.ts
@@ -1,4 +1,119 @@
 import { Component, OnInit } from '@angular/core';
+import { HistorialService } from "../../shared/services/historial.service";
+import { SafeUrlPipe } from "../../shared/pipes/safe-url.pipe";
+import { CommonModule, NgClass } from '@angular/common';
+import { PanelTitleComponent } from "../panel-title/panel-title.component";
+import { CardRealTimeComponent } from "../../core/card-real-time/card-real-time.component";
+import { DashboardPanelComponent } from "../../core/dashboard-panel/dashboard-panel.component";
+
+@Component({
+  selector: 'app-dashboard-historico',
+  templateUrl: './dashboard-historico.component.html',
+  standalone: true,
+  imports: [
+    SafeUrlPipe,
+    CommonModule,
+    PanelTitleComponent, NgClass,
+    CommonModule,
+    CardRealTimeComponent, DashboardPanelComponent,
+  ],
+  styleUrls: ['./dashboard-historico.component.css']
+})
+export class DashboardHistoricoComponent implements OnInit {
+
+  consumoMensual: string = '';
+  consumoDiario: string = '';
+  intensidadAmperaje: string = '';
+  potencia: string = '';
+  frecuencia: string = '';
+  alertasHistoricas: { tipo: string, descripcion: string }[] = [];
+  filtroSeleccionado: string = 'todos';
+
+  graficoSeleccionado: string = ''; // Variable para el gráfico seleccionado
+  graficoTitulo: string = ''; // Variable para el título del gráfico
+
+  constructor(private historialService: HistorialService) {}
+
+  ngOnInit(): void {
+    this.cargarConsumoUltimoMes();
+    this.cargarConsumoUltimoAnio();
+    this.cargarVoltaje();
+    this.cargarPotencia();
+    this.cargarAlertasHistoricas();
+  }
+
+  cargarConsumoUltimoMes(): void {
+    this.historialService.getConsumoUltimoMesGrafanaUrl().subscribe(url => {
+      this.consumoDiario = url;
+    });
+  }
+
+  cargarConsumoUltimoAnio(): void {
+    this.historialService.getConsumoUltimoAñoGrafanaUrl().subscribe(url => {
+      this.intensidadAmperaje = url;
+    });
+  }
+
+  cargarPotencia(): void {
+    this.historialService.getPotenciaGrafanaUrl().subscribe(url => {
+      this.potencia = url;
+    });
+  }
+
+  cargarVoltaje(): void {
+    this.historialService.getVoltajeGrafanaUrl().subscribe(url => {
+      this.frecuencia = url;
+    });
+  }
+
+  cargarAlertasHistoricas(): void {
+    this.historialService.getAlertasHistoricas().subscribe(alertas => {
+      this.alertasHistoricas = alertas;
+    });
+  }
+
+  aplicarFiltro(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.filtroSeleccionado = selectElement.value;
+  }
+
+  getAlertasFiltradas(): { tipo: string, descripcion: string }[] {
+    if (this.filtroSeleccionado === 'todos') {
+      return this.alertasHistoricas;
+    }
+    return this.alertasHistoricas.filter(alerta => alerta.tipo === this.filtroSeleccionado);
+  }
+
+  abrirModal(grafico: string) {
+    this.graficoSeleccionado = grafico;
+
+    switch (grafico) {
+      case 'consumoMensual':
+        this.graficoTitulo = 'Consumo Mensual Histórico';
+        break;
+      case 'amperaje':
+        this.graficoTitulo = 'Intensidad de Amperaje Histórico';
+        break;
+      case 'potencia':
+        this.graficoTitulo = 'Potencia Histórica';
+        break;
+      case 'frecuencia':
+        this.graficoTitulo = 'Frecuencia Histórica';
+        break;
+      default:
+        this.graficoTitulo = ''; // Título por defecto
+    }
+  }
+
+  cerrarModal() {
+    this.graficoSeleccionado = '';
+  }
+}
+
+
+/*
+
+import { Component, OnInit } from '@angular/core';
 import {HistorialService} from "../../shared/services/historial.service";
 import {SafeUrlPipe} from "../../shared/pipes/safe-url.pipe";
 import { CommonModule } from '@angular/common';
@@ -31,95 +146,96 @@ export class DashboardHistoricoComponent implements OnInit {
 
   constructor(private historialService: HistorialService) {}
 
-  ngOnInit(): void {
-    this.cargarConsumoMensual();
-    this.cargarConsumoDiario();
-    this.cargarIntensidadAmperaje();
-    this.cargarPotencia();
-    this.cargarFrecuencia();
-    this.cargarAlertasHistoricas();
-  }
+ngOnInit(): void {
+  this.cargarConsumoMensual();
+  this.cargarConsumoDiario();
+  this.cargarIntensidadAmperaje();
+  this.cargarPotencia();
+  this.cargarFrecuencia();
+  this.cargarAlertasHistoricas();
+}
 
-  cargarConsumoMensual(): void {
-    this.historialService.getConsumoMensual().subscribe(data => {
-      this.consumoMensual = data;
-    });
-  }
+cargarConsumoMensual(): void {
+  this.historialService.getConsumoMensual().subscribe(data => {
+    this.consumoMensual = data;
+  });
+}
 
-  cargarConsumoDiario(): void {
-    this.historialService.getConsumoDiario().subscribe(data => {
-      this.consumoDiario = data;
-    });
-  }
+cargarConsumoDiario(): void {
+  this.historialService.getConsumoDiario().subscribe(data => {
+    this.consumoDiario = data;
+  });
+}
 
-  cargarIntensidadAmperaje(): void {
-    this.historialService.getIntensidadAmperaje().subscribe(data => {
-      this.intensidadAmperaje = data;
-    });
-  }
+cargarIntensidadAmperaje(): void {
+  this.historialService.getIntensidadAmperaje().subscribe(data => {
+    this.intensidadAmperaje = data;
+  });
+}
 
-  cargarPotencia(): void {
-    this.historialService.getPotencia().subscribe(data => {
-      this.potencia = data;
-    });
-  }
+cargarPotencia(): void {
+  this.historialService.getPotencia().subscribe(data => {
+    this.potencia = data;
+  });
+}
 
-  cargarFrecuencia(): void {
-    this.historialService.getFrecuencia().subscribe(data => {
-      this.frecuencia = data;
-    });
-  }
+cargarFrecuencia(): void {
+  this.historialService.getFrecuencia().subscribe(data => {
+    this.frecuencia = data;
+  });
+}
 
-  cargarAlertasHistoricas(): void {
-    this.historialService.getAlertasHistoricas().subscribe(alertas => {
-      this.alertasHistoricas = alertas;
-    });
-  }
+cargarAlertasHistoricas(): void {
+  this.historialService.getAlertasHistoricas().subscribe(alertas => {
+    this.alertasHistoricas = alertas;
+  });
+}
 
-  loadAlertasHistoricas(): void {
-    this.historialService.getAlertasHistoricas().subscribe({
-      next: (data) => {
-        this.alertasHistoricas = data;
-      },
-      error: (err) => console.error('Error fetching alertas historicas:', err)
-    });
-  }
+loadAlertasHistoricas(): void {
+  this.historialService.getAlertasHistoricas().subscribe({
+    next: (data) => {
+      this.alertasHistoricas = data;
+    },
+    error: (err) => console.error('Error fetching alertas historicas:', err)
+  });
+}
 
-  aplicarFiltro(event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    this.filtroSeleccionado = selectElement.value;
-  }
+aplicarFiltro(event: Event): void {
+  const selectElement = event.target as HTMLSelectElement;
+  this.filtroSeleccionado = selectElement.value;
+}
 
-  getAlertasFiltradas(): { tipo: string, descripcion: string }[] {
-    if (this.filtroSeleccionado === 'todos') {
-      return this.alertasHistoricas;
-    }
-    return this.alertasHistoricas.filter(alerta => alerta.tipo === this.filtroSeleccionado);
+getAlertasFiltradas(): { tipo: string, descripcion: string }[] {
+  if (this.filtroSeleccionado === 'todos') {
+    return this.alertasHistoricas;
   }
+  return this.alertasHistoricas.filter(alerta => alerta.tipo === this.filtroSeleccionado);
+}
 
-  abrirModal(grafico: string) {
-    this.graficoSeleccionado = grafico;
+abrirModal(grafico: string) {
+  this.graficoSeleccionado = grafico;
 
-    switch (grafico) {
-      case 'consumoMensual':
-        this.graficoTitulo = 'Consumo Mensual Historico';
-        break;
-      case 'amperaje':
-        this.graficoTitulo = 'Intensidad de Amperaje Historico';
-        break;
-      case 'potencia':
-        this.graficoTitulo = 'Potencia Historica';
-        break;
-      case 'frecuencia':
-        this.graficoTitulo = 'Frecuencia Historica';
-        break;
-      default:
-        this.graficoTitulo = ''; // Título por defecto
-    }
+  switch (grafico) {
+    case 'consumoMensual':
+      this.graficoTitulo = 'Consumo Mensual Historico';
+      break;
+    case 'amperaje':
+      this.graficoTitulo = 'Intensidad de Amperaje Historico';
+      break;
+    case 'potencia':
+      this.graficoTitulo = 'Potencia Historica';
+      break;
+    case 'frecuencia':
+      this.graficoTitulo = 'Frecuencia Historica';
+      break;
+    default:
+      this.graficoTitulo = ''; // Título por defecto
   }
+}
 
-  cerrarModal() {
-    this.graficoSeleccionado = "";
-  }
+cerrarModal() {
+  this.graficoSeleccionado = "";
+}
 
 }
+*/
--- frontend/src/app/shared/services/historial.service.ts
+++ frontend/src/app/shared/services/historial.service.ts
@@ -1,4 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HistorialService {
+
+  constructor() {
+  }
+
+  private GRAFANA_BASE_URL = 'http://localhost:3000';
+
+
+ getConsumoUltimoMesGrafanaUrl(): Observable<string> {
+
+    const url = "${this.GRAFANA_BASE_URL}/d-solo/ae1m3p3ni09vke/hist-energy?orgId=1&panelId=1&var-deviceId=08:A6:F7:24:71:98&refresh=5s";
+    return of(url);
+  }
+
+
+  getConsumoUltimoAñoGrafanaUrl(): Observable<string> {
+
+    const url = "${this.GRAFANA_BASE_URL}/d-solo/fe1mcple571fkf/hist-energy-month?orgId=1&panelId=1&var-deviceId=08:A6:F7:24:71:98&refresh=5s";
+    return of(url);
+  }
+
+  getPotenciaGrafanaUrl(): Observable<string> {
+
+    const url = `${this.GRAFANA_BASE_URL}/d-solo/ee1me0bqeal8gf/power-last-year?orgId=1&panelId=1&var-deviceId=08:A6:F7:24:71:98&refresh=5s`;
+    return of(url);
+  }
+
+  getVoltajeGrafanaUrl(): Observable<string> {
+
+    const url = `${this.GRAFANA_BASE_URL}/d-solo/ee1me0bqeal8gf/power-last-year?orgId=1&panelId=1&var-deviceId=08:A6:F7:24:71:98&refresh=5s`;
+    return of(url);
+  }
+
+
+
+  getAlertasHistoricas(): Observable<{ tipo: string, descripcion: string }[]> {
+    const alertas = [
+      {tipo: 'corte', descripcion: 'Corte de energía registrado el 12/09/2024 a las 14:00'},
+      {tipo: 'dispositivo', descripcion: 'Nuevo dispositivo agregado el 05/08/2024: Aire Acondicionado'},
+      {tipo: 'corte', descripcion: 'Corte de energía registrado el 22/07/2024 a las 10:30'},
+      {tipo: 'dispositivo', descripcion: 'Nuevo dispositivo agregado el 18/06/2024: Lámpara LED'},
+      {tipo: 'dispositivo', descripcion: 'Nuevo dispositivo agregado el 11/06/2024: Microondas'}
+    ];
+    return of(alertas);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
