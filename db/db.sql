1c5ae84129e17d67fd5fa2451cfe600df731b1e0
alejandro <alcruz@alumno.unlam.edu.ar>
2024-10-22 20:07:42 -0300

    - added insert device on init

--- db/db.sql
+++ db/db.sql
@@ -4,6 +4,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
 CREATE TABLE `user` (
                         `id` int NOT NULL,
                         `email` varchar(255) DEFAULT NULL,
