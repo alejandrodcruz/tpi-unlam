f73ff31ef67f54e0ad6595f2429d20088dcd1603
alejandro <alcruz@alumno.unlam.edu.ar>
2024-10-24 23:57:28 -0300

    -- added energyCost Meaasurement

--- db/db.sql
+++ db/db.sql
@@ -33,30 +33,32 @@ CREATE TABLE `device` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Insertar datos en la tabla device
-INSERT INTO `device` (`device_id`, `assigned`, `estimated_consume`, `name`, `pairing_code`, `user_id`) VALUES
-('08:A6:F7:24:71:98', 1, 0, 'Heladera', NULL, 1),
-('08:A6:F7:24:71:99', 1, 0, 'Aire Acondicionado', NULL, 1),
-('08:A6:F7:24:71:9A', 1, 0, 'Microondas', NULL, 1),
-('08:A6:F7:24:71:9B', 1, 0, 'Lavarropas', NULL, 1),
-('08:A6:F7:24:71:9C', 1, 0, 'Televisor', NULL, 1),
-('08:A6:F7:24:71:9D', 1, 0, 'Computadora', NULL, 1),
-('08:A6:F7:24:71:9E', 1, 0, 'Lámpara LED', NULL, 1),
-('08:A6:F7:24:71:9F', 1, 0, 'Ventilador', NULL, 1),
-('08:A6:F7:24:71:A0', 1, 0, 'Cargador de Celular', NULL, 1),
-('08:A6:F7:24:71:A1', 1, 0, 'Router WiFi', NULL, 1),
-('08:A6:F7:24:71:A2', 1, 0, 'Lavavajillas', NULL, 1),
-('08:A6:F7:24:71:A3', 1, 0, 'Secador de Pelo', NULL, 1),
-('08:A6:F7:24:71:A4', 1, 0, 'Calefactor Eléctrico', NULL, 1),
-('08:A6:F7:24:71:A5', 1, 0, 'Plancha', NULL, 1),
-('08:A6:F7:24:71:A6', 1, 0, 'Cafetera', NULL, 1),
-('08:A6:F7:24:71:A7', 1, 0, 'Extractor de Aire', NULL, 1),
-('08:A6:F7:24:71:A8', 1, 0, 'Horno Eléctrico', NULL, 1),
-('08:A6:F7:24:71:A9', 1, 0, 'Bomba de Agua', NULL, 1),
-('08:A6:F7:24:71:AA', 1, 0, 'Radiador Eléctrico', NULL, 1),
-('08:A6:F7:24:71:AB', 1, 0, 'Impresora Láser', NULL, 1);
+SET @pairing_code_counter = 0;
+
+INSERT INTO `device` (`device_id`, `assigned`, `estimated_consume`, `name`, `pairing_code`, `user_id`)
+VALUES
+    ('08:A6:F7:24:71:98', 0, 0, 'Heladera', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:99', 0, 0, 'Aire Acondicionado', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9A', 0, 0, 'Microondas', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9B', 0, 0, 'Lavarropas', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9C', 0, 0, 'Televisor', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9D', 0, 0, 'Computadora', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9E', 0, 0, 'Lámpara LED', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:9F', 0, 0, 'Ventilador', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A0', 0, 0, 'Cargador de Celular', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A1', 0, 0, 'Router WiFi', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A2', 0, 0, 'Lavavajillas', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A3', 0, 0, 'Secador de Pelo', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A4', 0, 0, 'Calefactor Eléctrico', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A5', 0, 0, 'Plancha', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A6', 0, 0, 'Cafetera', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A7', 0, 0, 'Extractor de Aire', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A8', 0, 0, 'Horno Eléctrico', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:A9', 0, 0, 'Bomba de Agua', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:AA', 0, 0, 'Radiador Eléctrico', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL),
+    ('08:A6:F7:24:71:AB', 0, 0, 'Impresora Láser', LPAD((@pairing_code_counter := @pairing_code_counter + 1), 4, '0'), NULL);
 
 COMMIT;
-
 -- INSERT INTO `device` (`device_id`, `assigned`, `estimated_consume`, `name`, `pairing_code`, `user_id`) VALUES
 -- ('08:A6:F7:24:71:98', b'1', 0, 'Heladera', NULL, 1),
 -- ('08:A6:F7:24:71:99', b'1', 0, 'Aire Acondicionado', NULL, 1),
--- server/src/main/java/com/tpi/server/domain/models/TotalEnergyDetailedResponse.java
+++ server/src/main/java/com/tpi/server/domain/models/TotalEnergyDetailedResponse.java
@@ -8,20 +8,34 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TotalEnergyDetailedResponse {
     private double totalEnergy;
+    private double energyCost;  // Variable de costo
     private String deviceId; // Solo cuando se filtra por deviceId
     private Map<String, Double> devicesDetails; // Solo cuando no se filtra por deviceId
 
     public TotalEnergyDetailedResponse(double totalEnergy) {
         this.totalEnergy = totalEnergy;
+        this.energyCost = calculateEnergyCost(totalEnergy);
     }
 
     public TotalEnergyDetailedResponse(double totalEnergy, String deviceId) {
         this.totalEnergy = totalEnergy;
         this.deviceId = deviceId;
+        this.energyCost = calculateEnergyCost(totalEnergy);
     }
 
     public TotalEnergyDetailedResponse(double totalEnergy, Map<String, Double> devicesDetails) {
         this.totalEnergy = totalEnergy;
         this.devicesDetails = devicesDetails;
+        this.energyCost = calculateEnergyCost(totalEnergy);
+    }
+    // Calcular el costo de energía
+    private double calculateEnergyCost(double totalEnergy) {
+        if (totalEnergy < 250) { //Tarifa 1
+            return totalEnergy * 133;
+        } else if (totalEnergy <= 500) { //Tarifa 2
+            return totalEnergy * 150;
+        } else { // Tarifa 3
+            return totalEnergy * 160;
+        }
     }
 }
