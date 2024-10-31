2c956ae5c9c20160cb6adbea21fb9112e66b5587
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-31 01:39:02 -0300

    calculos del consumo de cada device

--- frontend/src/app/routes/consume/consume.component.ts
+++ frontend/src/app/routes/consume/consume.component.ts
@@ -5,6 +5,8 @@ import { DatePipe, CommonModule } from "@angular/common";
 import { PanelTitleComponent } from "../panel-title/panel-title.component";
 import {CardRealTimeComponent} from "../../core/card-real-time/card-real-time.component";
 import { Device, UserService } from '../../shared/services/user.service';
+import { AuthService } from '../../shared/services/auth.service';
+import { ConsumptionService } from '../../shared/services/consumption.service';
 
 
 
@@ -23,25 +25,30 @@ import { Device, UserService } from '../../shared/services/user.service';
 })
 export class ConsumeComponent implements OnInit {
   devices: Device[] = [];
+  userId: number;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private authService: AuthService,
+    private consumptionService: ConsumptionService)
+    {
+    this.userId = this.authService.getUserId() ?? 0;
+    }
 
   ngOnInit(): void {
     this.userService.getUserDevices().subscribe((devices) => {
       this.devices = devices;
       //test
       this.devices.forEach(device => {
-        this.userService.getLastDayConsumption(device.deviceId).subscribe(value => {
-          device.lastDayConsumption = value;
+        this.consumptionService.getLastDayConsumption(this.userId , device.deviceId).subscribe(value => {
+          device.lastDayConsumption = parseFloat(value.toFixed(2));
         });
-        this.userService.getCurrentMonthConsumption(device.deviceId).subscribe(value => {
-          device.currentMonthConsumption = value;
+        this.consumptionService.getCurrentMonthConsumption(this.userId ,device.deviceId).subscribe(value => {
+          device.currentMonthConsumption = parseFloat(value.toFixed(2));
         });
-        this.userService.getPreviousMonthConsumption(device.deviceId).subscribe(value => {
-          device.previousMonthConsumption = value;
+        this.consumptionService.getPreviousMonthConsumption(this.userId ,device.deviceId).subscribe(value => {
+          device.previousMonthConsumption = parseFloat(value.toFixed(2));
         });
-        this.userService.getProjectedCurrentMonthConsumption(device.deviceId).subscribe(value => {
-          device.projectedCurrentMonthConsumption = value;
+        this.consumptionService.getProjectedCurrentMonthConsumption(this.userId ,device.deviceId).subscribe(value => {
+          device.projectedCurrentMonthConsumption = parseFloat(value.toFixed(2));
         });
       });
       //test
