69ae771c181dbbfcd93b524c668582d266c67a15
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-10-31 23:26:09 -0300

    servicios de Address

--- frontend/src/app/shared/services/address.service.ts
+++ frontend/src/app/shared/services/address.service.ts
@@ -20,4 +20,16 @@ export class AddressService {
   getAddressesByUser(userId: number): Observable<Address[]> {
     return this.http.get<Address[]>(`${this.apiUrl}/user/${userId}`);
   }
+
+  addAddress(userId: number, address: Address): Observable<Address> {
+    return this.http.post<Address>(`${this.apiUrl}/user/${userId}`, address);
+  }
+
+  updateAddress(addressId: number, address: Address): Observable<Address> {
+    return this.http.put<Address>(`${this.apiUrl}/${addressId}`, address);
+  }
+
+  deleteAddress(addressId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${addressId}`);
+  }
 }
