1edea0c766fd9a54f430bd3d9f6316fee02ba289
alejandro <alcruz@alumno.unlam.edu.ar>
2024-11-01 11:44:03 -0300

    - added DeviceMapper.java / UserMapper.java - Change controller DeviceController.java / UserController.java

--- server/src/main/java/com/tpi/server/infrastructure/mappers/UserMapper.java
+++ server/src/main/java/com/tpi/server/infrastructure/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package com.tpi.server.infrastructure.mappers;
 
 import com.tpi.server.domain.models.User;
 import com.tpi.server.infrastructure.dtos.UserDTO;
+import com.tpi.server.infrastructure.dtos.UserResponseDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,5 +23,13 @@ public class UserMapper {
 
     public User toEntity(UserDTO userDTO) {
         return modelMapper.map(userDTO, User.class);
+    }
+
+    public UserResponseDTO toResponseDTO(User user) {
+        return modelMapper.map(user, UserResponseDTO.class);
+    }
+
+    public User toEntity(UserResponseDTO userResponseDTO) {
+        return modelMapper.map(userResponseDTO, User.class);
     }
 }
