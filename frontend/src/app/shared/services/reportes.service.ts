009d63a7e8184621fbef9b6c786c6dff8537ebe0
lucas2206reta <lureta@alumno.unlam.edu.ar>
2024-11-04 16:24:25 -0300

    Update reportes.service.ts

--- frontend/src/app/shared/services/reportes.service.ts
+++ frontend/src/app/shared/services/reportes.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
+import {HttpService} from "../utils/httpService";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportesService {
-  constructor(private http: HttpClient) {}
+
+  private controller = 'reports';
+
+  constructor(private httpService: HttpService) { }
+  //constructor(private http: HttpClient) {}
 
   getGraficoUrl(type: string, startDate: string, endDate: string): Observable<string> {
-
-    const url = `http://localhost:8080/reports/graphic-report `;
-
     const body = {
       type: type,
       startDate: startDate,
       endDate: endDate
     };
-
-    return this.http.post<string>(url, body).pipe(
+    return this.httpService.post<string>(body, `${this.controller}/graphic-report`).pipe(
       catchError((error) => {
         console.error('Error en getGraficoUrl:', error);
         return of('Error al obtener la URL del gráfico.');
